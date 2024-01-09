85f7cff159768eac10e53fdc6c6a094e173a921f
Juschwy <jul.fehlmann@gmail.com>
2024-01-09 16:13:13 +0100

    Add customizable time format

--- src/components/Apps/Settings/Settings.tsx
+++ src/components/Apps/Settings/Settings.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks.ts";
-import { setAppBarWidth, setInfoBarHeight } from "../../../features/personalisation/PersonalisationSlice.ts";
+import { setAppBarWidth, setInfoBarHeight, setInfoBarTimeFormat } from "../../../features/personalisation/PersonalisationSlice.ts";
 
 
 function Settings() {
@@ -23,6 +23,12 @@ function Settings() {
         type="number"
         value={useAppSelector((state) => state.personalisation.infoBarHeight)}
         onChange={(e) => dispatch(setInfoBarHeight(e.target.valueAsNumber))}
+      />
+      <p>InfoBarTimeFormat (use <a target={"_blank"} href="https://day.js.org/docs/en/display/format">this format</a>)</p>
+      <input
+        type="text"
+        value={useAppSelector((state) => state.personalisation.infoBarTimeFormat)}
+        onChange={(e) => dispatch(setInfoBarTimeFormat(e.target.value))}
       />
     </>
   );
