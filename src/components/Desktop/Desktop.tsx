ab51321e6bd59364ab8d6ceff47dc815de154c00
Juschwy <73661431+Juschwy@users.noreply.github.com>
2024-01-09 15:45:48 +0100

    Add desktop select (#15)

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -1,6 +1,6 @@
 import App from "./App/App.tsx";
 import "./Desktop.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AppConfigType } from "./AppConfigType.ts";
 
 type DesktopProps = {
@@ -8,8 +8,22 @@ type DesktopProps = {
   setOpenedAppConfigs: React.Dispatch<React.SetStateAction<AppConfigType[]>>
 }
 
+type DesktopSelectPos = {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+}
+
 function Desktop({ openedAppConfigs, setOpenedAppConfigs }: DesktopProps) {
   const [isFullscreenPreview, setIsFullscreenPreview] = useState<boolean>(false);
+  const [startDesktopSelectPos, setStartDesktopSelectPos] = useState<number[] | undefined>()
+  const [desktopSelectPos, setDesktopSelectPos] = useState<DesktopSelectPos>({
+    x: -1,
+    y: -1,
+    height: 0,
+    width: 0
+  });
 
   useEffect(() => {
     console.log(openedAppConfigs);
@@ -42,9 +56,52 @@ function Desktop({ openedAppConfigs, setOpenedAppConfigs }: DesktopProps) {
     });
   };
 
+  const startDesktopSelect = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setStartDesktopSelectPos([e.clientY, e.clientX])
+  };
+
+  const updateDesktopSelect = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    if (startDesktopSelectPos){
+      setDesktopSelectPos(desktopSelectPos => {
+        const tmp = {...desktopSelectPos}
+        if (e.clientY < startDesktopSelectPos[0]){
+          tmp.y = e.clientY
+          tmp.height = startDesktopSelectPos[0] - e.clientY
+        }
+        else {
+          tmp.y = startDesktopSelectPos[0]
+          tmp.height = e.clientY - startDesktopSelectPos[0]
+        }
+        if (e.clientX < startDesktopSelectPos[1]){
+          tmp.x = e.clientX
+          tmp.width = startDesktopSelectPos[1] - e.clientX
+        }
+        else {
+          tmp.x = startDesktopSelectPos[1]
+          tmp.width = e.clientX - startDesktopSelectPos[1]
+        }
+        return tmp
+      });
+    }
+  };
+
+  const endDesktopSelect = () => {
+    setStartDesktopSelectPos(undefined)
+    setDesktopSelectPos({ x: -1, y: -1, height: 0, width: 0 });
+  };
+
   return (
-    <main id={"desktop"}>
+    <main id={"desktop"}
+          onMouseDown={e => startDesktopSelect(e)}
+          onMouseMove={e => updateDesktopSelect(e)}
+          onMouseUp={endDesktopSelect}>
       <div id={"fullscreenPreview"} className={isFullscreenPreview ? "active" : ""} />
+      <div id={"desktopSelect"} style={{
+        top: (desktopSelectPos.y + "px"),
+        left: (desktopSelectPos.x + "px"),
+        height: (desktopSelectPos.height + "px"),
+        width: (desktopSelectPos.width + "px")
+      }} />
 
       {
         openedAppConfigs.map((appConfig, i) => (
