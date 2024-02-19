b781ec1da35eb772315ba3068a6fde1a621fff49
Juschwy <jul.fehlmann@gmail.com>
2024-02-19 14:18:45 +0100

    Add examples

--- src/components/Apps/FileExplorer/FileExplorer.tsx
+++ src/components/Apps/FileExplorer/FileExplorer.tsx
@@ -101,6 +101,38 @@ export default function FileExplorer() {
         <button onClick={createDirectory}>Create Dir</button>
         <button onClick={createFile}>Create File</button>
       </div>
+      <details>
+        <summary>Advanced Features (currently only available in debug console)</summary>
+        <p>The "fs." or "window.fs" can be used to access the filesystem and some functions:</p>
+        <ul>
+          <li>"fs" ={">"} the filesystem, that must be provided on any other method call</li>
+          <li>createDirectoryEntry ={">"} creates a directory</li>
+          <li>createFileEntry ={">"} creates a file</li>
+          <li>loadFilesystem ={">"} loads the filesystem (use the already loaded filesystem at "fs.fs" or
+            "window.fs.fs"
+          </li>
+          <li>getDirectoryEntry ={">"} get directory (as object)</li>
+          <li>getFileEntry ={">"} get file (as object)</li>
+          <li>getEntryMetadata ={">"} get the metadata of a entry (file or directory)</li>
+          <li>readDirectoryEntry ={">"} read the entries in a dir</li>
+          <li>readFileEntry ={">"} read the text in a file</li>
+          <li>writeFileEntry ={">"} write blob to a file</li>
+          <li>writeFileString</li>
+          <li>copyEntryTo ={">"} copy entry to other directory</li>
+          <li>getEntryURL ={">"} get url to file, can be used in image tags (and others, like normal source link)</li>
+          <li>moveEntryTo ={">"} move entry to other directory</li>
+          <li>removeEntry ={">"} remove entry (directory or file)</li>
+          <li>removeDirectoryRecursively ={">"} remove directory and all its sub-entries (files and directories)</li>
+        </ul>
+        <details>
+          <summary>Examples</summary>
+          <p>fs.createDirectoryEntry("/testDir", fs.fs).then(console.log)</p>
+          <p>fs.createFileEntry("/testDir/testFile.txt", fs.fs).then(console.log)</p>
+          <p>fs.readDirectoryEntry("/testDir", fs.fs).then(console.log)</p>
+          <p>fs.writeFileString("/testDir/testFile.txt", "hello world", fs.fs).then(console.log)</p>
+          <p>fs.readFileEntry("/testDir/testFile.txt", fs.fs).then(console.log)</p>
+        </details>
+      </details>
     </div>
   );
 }
